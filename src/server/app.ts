f7c94f7ff82e3ccd3c174080bb2057fc2ac8b342
Isabel Sáenz <isasaete@gmail.com>
2026-01-28 13:03:33 +0100

    Add handleError endpoint to app

--- src/server/app.ts
+++ src/server/app.ts
@@ -1,11 +1,14 @@
 import express from "express";
 import morgan from "morgan";
 import checkHealthStatus from "../middleware/checkHealthStatus/checkHealthStatus.js";
+import handleErrors from "../middleware/handleError/handleErrors.js";
 
 const app = express();
 
 app.use(morgan("dev"));
 
 app.get("/", checkHealthStatus);
+
+app.use(handleErrors);
 
 export default app;
--- src/utils/globals/globals.ts
+++ src/utils/globals/globals.ts
@@ -1,8 +1,8 @@
-import { StatusCode } from "./types.js";
-
-const statusCode: StatusCode = {
+const statusCode = {
   OK: 200,
   INTERNAL_SERVER_ERROR: 500,
-};
+} as const;
+
+export type StatusCodeKey = keyof typeof statusCode;
 
 export default statusCode;
